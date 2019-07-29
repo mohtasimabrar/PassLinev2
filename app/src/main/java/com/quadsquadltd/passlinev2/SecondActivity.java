dbc67c21afdc3036cf374fdc0e4229ee25da0e95
madjohnny89 <madjohnny89@gmail.com>
2019-07-29 15:15:49 +0600

    Solved the problem of Button activity method.Now just correct your algorithm(I provided a comment below in the second Activity Java class) and follow the button activity method for the other buttons too

--- app/src/main/java/com/quadsquadltd/passlinev2/SecondActivity.java
+++ app/src/main/java/com/quadsquadltd/passlinev2/SecondActivity.java
@@ -8,6 +8,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class SecondActivity extends Activity {
+    Button nrzI;
+    Button nrzL;
+    Button nrzUni;
+    Button mlt3;
+    Button ami;
+    Button man;
+    Button dman;
+    ImageView bitOne;
+    ImageView bitTwo;
+    ImageView bitThree;
+    ImageView bitFour;
+    ImageView bitFive;
+    ImageView bitSix;
+    ImageView bitSeven;
+    ImageView bitEight;
+    ImageView bitNine;
+    ImageView bitTen;
+    String bitStream;
 
 
     @Override
@@ -16,26 +34,26 @@ public class SecondActivity extends Activity {
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        final String bitStream = intent.getStringExtra(MainActivity.bitStream);
+        bitStream = intent.getStringExtra(MainActivity.bitStream);
 
-        Button nrzI = (Button) findViewById(R.id.nrzIButton);
-        Button nrzL = (Button) findViewById(R.id.nrzLButton);
-        Button nrzUni = (Button) findViewById(R.id.nrzUniButton);
-        Button mlt3 = (Button) findViewById(R.id.mlt3Button);
-        Button ami = (Button) findViewById(R.id.amiButton);
-        Button man = (Button) findViewById(R.id.manButton);
-        Button dman = (Button) findViewById(R.id.dmanButton);
+        nrzI = (Button) findViewById(R.id.nrzIButton);
+        nrzL = (Button) findViewById(R.id.nrzLButton);
+        nrzUni = (Button) findViewById(R.id.nrzUniButton);
+        mlt3 = (Button) findViewById(R.id.mlt3Button);
+        ami = (Button) findViewById(R.id.amiButton);
+        man = (Button) findViewById(R.id.manButton);
+        dman = (Button) findViewById(R.id.dmanButton);
 
-        final ImageView bitOne = findViewById(R.id.bitOne);
-        final ImageView bitTwo = findViewById(R.id.bitTwo);
-        final ImageView bitThree = findViewById(R.id.bitThree);
-        final ImageView bitFour = findViewById(R.id.bitFour);
-        final ImageView bitFive = findViewById(R.id.bitFive);
-        final ImageView bitSix = findViewById(R.id.bitSix);
-        final ImageView bitSeven = findViewById(R.id.bitSeven);
-        final ImageView bitEight = findViewById(R.id.bitEight);
-        final ImageView bitNine = findViewById(R.id.bitNine);
-        final ImageView bitTen = findViewById(R.id.bitTen);
+        ImageView bitOne = findViewById(R.id.bitOne);
+        ImageView bitTwo = findViewById(R.id.bitTwo);
+        ImageView bitThree = findViewById(R.id.bitThree);
+        ImageView bitFour = findViewById(R.id.bitFour);
+        ImageView bitFive = findViewById(R.id.bitFive);
+        ImageView bitSix = findViewById(R.id.bitSix);
+        ImageView bitSeven = findViewById(R.id.bitSeven);
+        ImageView bitEight = findViewById(R.id.bitEight);
+        ImageView bitNine = findViewById(R.id.bitNine);
+        ImageView bitTen = findViewById(R.id.bitTen);
 
         nrzI.setOnClickListener(new View.OnClickListener() {
 
@@ -66,7 +84,7 @@ public class SecondActivity extends Activity {
             }
         });
 
-        ami.setOnClickListener(new View.OnClickListener() {
+     /*   ami.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int tracker=1;
@@ -100,7 +118,8 @@ public class SecondActivity extends Activity {
                 bitNine.setImageResource(print[8]);
                 bitTen.setImageResource(print[9]);
             }
-        });
+        }); */
+
 
         man.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,4 +135,39 @@ public class SecondActivity extends Activity {
             }
         });
     }
-}
+
+    public void amiDO(View mView) {
+
+            int tracker = 1;
+            char[] bitStreamCopy = bitStream.toCharArray();
+            int[] print = new int[10];
+            for (int i = 0; i < bitStreamCopy.length; i++) {
+
+                if (bitStreamCopy[i] == 0)
+                    print[i] = R.drawable.mid;
+
+                if (bitStreamCopy[i] == 1 && tracker % 2 != 0) {
+                    print[i] = R.drawable.up;
+                    tracker++;
+                }
+
+                if (bitStreamCopy[i] == 1 && tracker % 2 == 0) {
+                    print[i] = R.drawable.down;
+                    tracker++;
+                }
+            }
+            // Here's a null pointer exception there must be a problem with your algorithm
+            bitOne.setImageResource(print[0]);
+            bitTwo.setImageResource(print[1]);
+            bitThree.setImageResource(print[2]);
+            bitFour.setImageResource(print[3]);
+            bitFive.setImageResource(print[4]);
+            bitSix.setImageResource(print[5]);
+            bitSeven.setImageResource(print[6]);
+            bitEight.setImageResource(print[7]);
+            bitNine.setImageResource(print[8]);
+            bitTen.setImageResource(print[9]);
+
+        }
+    }
+
