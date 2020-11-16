826e3eb3c64851ad933bf77bc2982f45f723bba7
mohtasimabrar <mohtasim.abrar@gmail.com>
2020-11-16 23:22:14 +0600

    Working Patch from PC without new UI

--- app/src/main/java/com/quadsquadltd/passlinev2/MainActivity.java
+++ app/src/main/java/com/quadsquadltd/passlinev2/MainActivity.java
@@ -1,21 +1,20 @@
 package com.quadsquadltd.passlinev2;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 public class MainActivity extends Activity {
-
     public static final String bitStream = "QuadSquadLtd.Application.bitStream";
-
+    String lengthError = "The bit stream contains more than 10 bits";
+    String binaryError = "The bit stream contains numbers other than 0 and 1";
+    String inputError = "No input detected";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button button = (Button) findViewById(R.id.input_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,12 +24,36 @@ public class MainActivity extends Activity {
         });
     }
     public void openSecondActivity(){
-
         EditText input = (EditText) findViewById(R.id.bit_stream);
         String bit_Stream = input.getText().toString();
-
-        Intent intent = new Intent(this,SecondActivity.class);
-        intent.putExtra(bitStream,bit_Stream);
-        startActivity(intent);
+        if (bit_Stream.length() > 10){
+            Toast.makeText(getApplicationContext(),lengthError,Toast.LENGTH_LONG).show();
+        } else if (bit_Stream.length() <= 0){
+            Toast.makeText(getApplicationContext(),inputError,Toast.LENGTH_LONG).show();
+        } else {
+            char [] bs = bit_Stream.toCharArray();
+            boolean binary = false;
+            for (int i = 0; i< bs.length;i++){
+                if (bs[i]== '1'|| bs[i]=='0'){
+                    binary = true;
+                } else {
+                    binary = false;
+                    break;
+                }
+            }
+            if (binary) {
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra(bitStream, bit_Stream);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(),binaryError,Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
+
+
+
+
+
+
--- app/src/main/java/com/quadsquadltd/passlinev2/SecondActivity.java
+++ app/src/main/java/com/quadsquadltd/passlinev2/SecondActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SecondActivity extends Activity {
     Button nrzI;
@@ -26,6 +27,9 @@ public class SecondActivity extends Activity {
     ImageView bitNine;
     ImageView bitTen;
     String bitStream;
+    TextView bitText;
+
+
 
 
     @Override
@@ -44,119 +48,49 @@ public class SecondActivity extends Activity {
         man = (Button) findViewById(R.id.manButton);
         dman = (Button) findViewById(R.id.dmanButton);
 
-        ImageView bitOne = findViewById(R.id.bitOne);
-        ImageView bitTwo = findViewById(R.id.bitTwo);
-        ImageView bitThree = findViewById(R.id.bitThree);
-        ImageView bitFour = findViewById(R.id.bitFour);
-        ImageView bitFive = findViewById(R.id.bitFive);
-        ImageView bitSix = findViewById(R.id.bitSix);
-        ImageView bitSeven = findViewById(R.id.bitSeven);
-        ImageView bitEight = findViewById(R.id.bitEight);
-        ImageView bitNine = findViewById(R.id.bitNine);
-        ImageView bitTen = findViewById(R.id.bitTen);
+        bitOne = findViewById(R.id.bitOne);
+        bitTwo = findViewById(R.id.bitTwo);
+        bitThree = findViewById(R.id.bitThree);
+        bitFour = findViewById(R.id.bitFour);
+        bitFive = findViewById(R.id.bitFive);
+        bitSix = findViewById(R.id.bitSix);
+        bitSeven = findViewById(R.id.bitSeven);
+        bitEight = findViewById(R.id.bitEight);
+        bitNine = findViewById(R.id.bitNine);
+        bitTen = findViewById(R.id.bitTen);
 
-        nrzI.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        nrzL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        nrzUni.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        mlt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-     /*   ami.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int tracker=1;
-                char[] bitStreamCopy = bitStream.toCharArray();
-                int [] print = new int[10];
-                for(int i=0;i<bitStreamCopy.length;i++){
-
-                    if(bitStreamCopy[i]==0)
-                        print[i]= R.drawable.mid;
-
-                    if(bitStreamCopy[i]==1 && tracker%2!=0)
-                    {
-                        print[i]=R.drawable.up;
-                        tracker++;
-                    }
-
-                    if(bitStreamCopy[i]==1 && tracker%2==0)
-                    {
-                        print[i]=R.drawable.down;
-                        tracker++;
-                    }
-                }
-                bitOne.setImageResource(print[0]);
-                bitTwo.setImageResource(print[1]);
-                bitThree.setImageResource(print[2]);
-                bitFour.setImageResource(print[3]);
-                bitFive.setImageResource(print[4]);
-                bitSix.setImageResource(print[5]);
-                bitSeven.setImageResource(print[6]);
-                bitEight.setImageResource(print[7]);
-                bitNine.setImageResource(print[8]);
-                bitTen.setImageResource(print[9]);
-            }
-        }); */
+        bitText = findViewById(R.id.bitText);
 
 
-        man.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        dman.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
     public void amiDO(View mView) {
 
             int tracker = 1;
             char[] bitStreamCopy = bitStream.toCharArray();
-            int[] print = new int[10];
+            int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                    R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                    R.drawable.empty,R.drawable.empty};
             for (int i = 0; i < bitStreamCopy.length; i++) {
-
-                if (bitStreamCopy[i] == 0)
+                if (bitStreamCopy[i] == '0')
                     print[i] = R.drawable.mid;
 
-                if (bitStreamCopy[i] == 1 && tracker % 2 != 0) {
+                else if (bitStreamCopy[i] == '1' && tracker % 2 != 0) {
                     print[i] = R.drawable.up;
                     tracker++;
                 }
 
-                if (bitStreamCopy[i] == 1 && tracker % 2 == 0) {
+                else if (bitStreamCopy[i] == '1' && tracker % 2 == 0) {
                     print[i] = R.drawable.down;
                     tracker++;
                 }
+                else {
+                    print[i]=R.drawable.empty;
+                }
             }
-            // Here's a null pointer exception there must be a problem with your algorithm
+
+            bitText.setText(bitStream);
+
             bitOne.setImageResource(print[0]);
             bitTwo.setImageResource(print[1]);
             bitThree.setImageResource(print[2]);
@@ -167,7 +101,239 @@ public class SecondActivity extends Activity {
             bitEight.setImageResource(print[7]);
             bitNine.setImageResource(print[8]);
             bitTen.setImageResource(print[9]);
-
-        }
     }
+
+    public void nrzIDO (View mView) {
+        int prev = R.drawable.empty;
+        int tracker = 0;
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        if (bitStreamCopy[0] == '0') {
+            print[0] = R.drawable.up;
+            prev = R.drawable.up;
+        } else {
+            print[0] = R.drawable.down;
+            prev = R.drawable.down;
+            tracker++;
+        }
+        for (int i = 1; i < bitStreamCopy.length; i++) {
+
+            if (bitStreamCopy[i] == '0') {
+                print[i] = prev;
+            } else if (tracker % 2 != 0) {
+                print[i] = R.drawable.up;
+                prev = R.drawable.up;
+                tracker++;
+            } else if (tracker % 2 == 0) {
+                print[i] = R.drawable.down;
+                prev = R.drawable.down;
+                tracker++;
+            }
+        }
+        bitText.setText(bitStream);
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+
+    public void nrzLDO (View mView){
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        for (int i = 0; i <bitStreamCopy.length; i++) {
+            if (bitStreamCopy[i]=='0') {
+                print[i]=R.drawable.up;
+            }
+            else if (bitStreamCopy[i] == '1') {
+                print[i]=R.drawable.down;
+            }
+            else {
+                print[i]=R.drawable.empty;
+            }
+        }
+
+        bitText.setText(bitStream);
+
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+    public void nrzUniDO (View mView){
+
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        for (int i = 0; i < bitStreamCopy.length; i++) {
+            if (bitStreamCopy[i]=='0') {
+                print[i]=R.drawable.mid;
+            }
+            else if (bitStreamCopy[i] == '1') {
+                print[i]=R.drawable.up;
+            }
+            else {
+                print[i]=R.drawable.empty;
+            }
+        }
+
+        bitText.setText(bitStream);
+
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+
+    public void mlt3DO (View mView){
+        int prev = R.drawable.empty;
+        int tracker = 0;
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        if(bitStreamCopy[0]=='0'){
+            print[0]=R.drawable.mid;
+            prev=R.drawable.mid;
+        }
+        else if (bitStreamCopy[0]=='1'){
+            print[0]=R.drawable.up;
+            prev=R.drawable.up;
+        }
+
+        for(int i=1;i<bitStreamCopy.length;i++) {
+
+            if(bitStreamCopy[i]=='0') {
+                print[i] = prev;
+            }
+            else if(prev == R.drawable.up){
+                print[i]= R.drawable.mid;
+                prev=R.drawable.mid;
+                tracker++;
+            }
+            else if(prev == R.drawable.mid && tracker%2!=0){
+                print[i]=R.drawable.down;
+                prev=R.drawable.down;
+            }
+            else if(prev == R.drawable.down){
+                print[i]=R.drawable.mid;
+                prev= R.drawable.mid;
+                tracker++;
+            }
+            else if(prev == R.drawable.mid && tracker%2==0){
+                print[i]=R.drawable.up;
+                prev=R.drawable.up;
+            }
+        }
+
+        bitText.setText(bitStream);
+
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+    public void manDO (View mView){
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        for (int i = 0; i < bitStreamCopy.length; i++) {
+            if (bitStreamCopy[i]=='0') {
+                print[i]=R.drawable.mandown;
+            }
+            else if (bitStreamCopy[i] == '1') {
+                print[i]=R.drawable.manup;
+            }
+            else {
+                print[i]=R.drawable.empty;
+            }
+        }
+
+        bitText.setText(bitStream);
+
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+    public void dmanDO (View mView){
+        int prev = R.drawable.empty;
+        int tracker = 0;
+        char[] bitStreamCopy = bitStream.toCharArray();
+        int[] print = {R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty,R.drawable.empty,R.drawable.empty,
+                R.drawable.empty,R.drawable.empty};
+        if (bitStreamCopy[0] == '0') {
+            print[0] = R.drawable.manup;
+            prev = R.drawable.manup;
+        } else {
+            print[0] = R.drawable.mandown;
+            prev = R.drawable.mandown;
+            tracker++;
+        }
+        for (int i = 1; i < bitStreamCopy.length; i++) {
+
+            if (bitStreamCopy[i] == '0') {
+                print[i] = prev;
+            } else if (tracker % 2 != 0) {
+                print[i] = R.drawable.manup;
+                prev = R.drawable.manup;
+                tracker++;
+            } else if (tracker % 2 == 0) {
+                print[i] = R.drawable.mandown;
+                prev = R.drawable.mandown;
+                tracker++;
+            }
+        }
+
+        bitText.setText(bitStream);
+
+        bitOne.setImageResource(print[0]);
+        bitTwo.setImageResource(print[1]);
+        bitThree.setImageResource(print[2]);
+        bitFour.setImageResource(print[3]);
+        bitFive.setImageResource(print[4]);
+        bitSix.setImageResource(print[5]);
+        bitSeven.setImageResource(print[6]);
+        bitEight.setImageResource(print[7]);
+        bitNine.setImageResource(print[8]);
+        bitTen.setImageResource(print[9]);
+    }
+
+}
 
